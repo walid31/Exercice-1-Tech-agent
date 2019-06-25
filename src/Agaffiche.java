c77e31accaf9115e8f1b8572ee4a1e2a88e5f463
Walid31 <ghalemwalid31@gmail.com>
2019-06-25 20:07:39 +0200

    finished creating agent

---
+++ src/Agaffiche.java
@@ -0,0 +1,11 @@
+import jade.core.Agent;
+
+public class Agaffiche extends Agent{
+	
+		protected void setup() {
+		System.out.println("Bonjour mon nom est "+getLocalName());
+		//doDelete();
+	//	Runtime.getRuntime().exit(Testeur.AP_DELETED);
+	}
+}
+
